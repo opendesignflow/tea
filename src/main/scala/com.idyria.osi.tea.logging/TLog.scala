33a22d270ae276114892cfb66990b53401a4a50b
rleys <richard.leys@ziti.uni-heidelberg.de>
2013-08-30 09:36:30 +0200

    Added base interface for logging using TLogSource trait

--- src/main/scala/com.idyria.osi.tea.logging/TLog.scala
+++ src/main/scala/com.idyria.osi.tea.logging/TLog.scala
@@ -1,28 +1,60 @@
 package com.idyria.osi.tea.logging
 
-object TLog {
-
-    // Logger management
-    //-------------------------
+trait TLogSource {
 
 
-    // Log Interfaces
-    //----------------------
 
+    def logError( msg : => String ) : Unit = {
 
-    def logInfo( msg : String ) = {
+        TLog.doLog("",TLog.ERROR,{ () => msg})
 
     }
+
+    def logWarn( msg : => String ) : Unit = {
+
+        TLog.doLog("",TLog.WARNING,{ () => msg})
+
+    }
+
 
     /**
         Log Infos based on a closure that will be executed only if the log Level is allowed
     */
-    def logInfo( cl : => String ) = { cl }
+    def logInfo( msg : => String ) : Unit = {
 
-
-    def logFine( msg : String ) = {
+        TLog.doLog("",TLog.INFO,{ () => msg})
 
     }
+
+
+    def logFine( msg : => String ) : Unit = {
+
+        TLog.doLog("",TLog.FINE,{ () => msg})
+
+    }
+
+    def logFull( msg : => String ) : Unit = {
+
+        TLog.doLog("",TLog.FULL,{ () => msg})
+
+    }
+    
+     
+}
+
+object TLog extends Enumeration {
+
+    type Level = Value
+    val FATAL, ERROR, WARNING, INFO, FINE, FULL = Value
+
+    // Logger management
+    //-------------------------
+
+    def doLog(realm: String,level: Level,message: () => String) : Unit = {
+
+    }
+
+
 
 
 }

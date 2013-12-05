92a3902bc91c81bd2fe1d33a2e00fb46777db2cb
rleys <richard.leys@ziti.uni-heidelberg.de>
2013-12-05 09:12:55 +0100

    Added createThread to thread language. The createdThread must be started by user

--- src/main/scala/com.idyria.osi.tea.thread/ThreadLanguage.scala
+++ src/main/scala/com.idyria.osi.tea.thread/ThreadLanguage.scala
@@ -7,14 +7,25 @@ trait ThreadLanguage {
   
   def fork(cl: => Any) : Thread = {
     
+    //-- Create Thread in local thread group
     var th = new Thread(new Runnable {
       def run() = {
         cl
       }
     })
+    //th.getThreadGroup()
     th.start
     th
     
+  }
+  
+  def createThread (cl: => Any) : Thread = {
+    var th = new Thread(new Runnable {
+      def run() = {
+        cl
+      }
+    })
+    th
   }
   
 }

d95c910fe1bf86c2c87e6111938bfeefd130b177
rleys <richard.leys@ziti.uni-heidelberg.de>
2014-08-06 11:14:30 +0200

    Updated Thread language

--- src/main/scala/com.idyria.osi.tea.thread/ThreadLanguage.scala
+++ src/main/scala/com.idyria.osi.tea.thread/ThreadLanguage.scala
@@ -19,6 +19,25 @@ trait ThreadLanguage {
     
   }
   
+  /**
+   * Forks the provided closure in a Thread and waits for it to end
+   */
+  def forkJoin[T](cl: => T) :T = {
+    
+    //-- Create Thread in local thread group
+    var res : Any = null
+    var th = new Thread(new Runnable {
+      def run() = {
+        res = cl
+      }
+    })
+    //th.getThreadGroup()
+    th.start
+    th.join
+    
+    res.asInstanceOf[T]
+  }
+  
   def createThread (cl: => Any) : Thread = {
     var th = new Thread(new Runnable {
       def run() = {

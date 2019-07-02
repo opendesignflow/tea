2c34af380dbc0b54e1965646974a7e47aefc1368
Richard Leys <leys@witte-group.de>
2019-07-02 16:49:39 +0200

    Added Hash Utilities in scala version

---
+++ src/main/scala/com/idyria/osi/tea/security/TSecUtils.scala
@@ -0,0 +1,27 @@
+package com.idyria.osi.tea.security
+
+import java.security.MessageDigest
+
+object TSecUtils {
+
+    def hashBytes(bytes: Array[Byte], alg: String) = {
+        // Create the message digest algorithm
+        var digest = MessageDigest.getInstance(alg);
+
+        digest.update(bytes);
+
+        // Digest and return
+        digest.digest();
+    }
+
+    def hashBytesToHexString(bytes: Array[Byte], alg: String,join:String=":") = {
+
+        
+        hashBytes(bytes,alg).map {
+            b => 
+                Math.abs(b.toInt).toHexString.padTo(2, 0).mkString
+        }.mkString(join)
+        
+    }
+
+}
